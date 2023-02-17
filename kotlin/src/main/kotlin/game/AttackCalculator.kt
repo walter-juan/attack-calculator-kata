b3c3393c1c025e61397aa90136f0cc8297ef2f90
Walter Juan <4141614+walter-juan@users.noreply.github.com>
2023-02-17 15:54:01 +0100

    Peel technique, and add more tests

--- kotlin/src/main/kotlin/game/AttackCalculator.kt
+++ kotlin/src/main/kotlin/game/AttackCalculator.kt
@@ -7,8 +7,12 @@ class AttackCalculator {
     private val random = Random
 
     fun calculateDamage(atk: Character, def: Character): Int {
-        val defaultAttack = atk.force
         val dice = random.nextInt(1..20)
+        return calculateDamage(atk = atk, def = def, dice = dice)
+    }
+
+    fun calculateDamage(atk: Character, def: Character, dice: Int): Int {
+        val defaultAttack = atk.force
         val currentAttack = defaultAttack + dice
         var damage = atk.damageDealt
 
--- kotlin/src/test/kotlin/game/AttackCalculatorTests.kt
+++ kotlin/src/test/kotlin/game/AttackCalculatorTests.kt
@@ -4,6 +4,7 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
 class AttackCalculatorTests : StringSpec({
+
     "defender is stronger enough to defend from damage" {
         val attacker = Character(armorClass = 0, weaponDamage = 10, race = "elf", force = 1)
         val defender = Character(armorClass = 22, weaponDamage = 10, race = "dwarf", force = 0)
@@ -11,5 +12,37 @@ class AttackCalculatorTests : StringSpec({
         val attackCalculator = AttackCalculator()
         val damage = attackCalculator.calculateDamage(atk = attacker, def = defender)
         damage shouldBe 0
+    }
+
+    "attacker is stronger enough to do damage but dice is one means no damage" {
+        val attacker = Character(armorClass = 0, weaponDamage = 10, race = "elf", force = 1)
+        val defender = Character(armorClass = 22, weaponDamage = 10, race = "dwarf", force = 0)
+
+        var attackCalculator = AttackCalculator()
+        var result = attackCalculator.calculateDamage(attacker, defender, 1)
+
+        result shouldBe 0
+    }
+
+    "attacker is stronger enough to do damage and dice is twenty means double damage" {
+        var attackerWeaponDamage = 10
+        var attacker = Character(0, attackerWeaponDamage, "elf", 80)
+        var defender = Character(1, 10, "dwarf", 0)
+
+        var attackCalculator = AttackCalculator()
+        var result = attackCalculator.calculateDamage(attacker, defender, 20)
+
+        result shouldBe (attackerWeaponDamage * 2)
+    }
+
+    "attacker is stronger enough to do damage and dice is not one and twenty means default damage" {
+        var attackerWeaponDamage = 10
+        var attacker = Character(0, attackerWeaponDamage, "elf", 80)
+        var defender = Character(1, 10, "dwarf", 0)
+
+        var attackCalculator = AttackCalculator()
+        var result = attackCalculator.calculateDamage(attacker, defender, 2)
+
+        result shouldBe (attackerWeaponDamage)
     }
 })
