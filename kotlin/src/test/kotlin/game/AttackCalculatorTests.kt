e14d1f3ea212014a6b1d6c139fb32a00dc1f0ebd
Walter Juan <4141614+walter-juan@users.noreply.github.com>
2023-02-17 15:45:31 +0100

    Create the first tests

--- kotlin/src/test/kotlin/game/AttackCalculatorTests.kt
+++ kotlin/src/test/kotlin/game/AttackCalculatorTests.kt
@@ -4,11 +4,12 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
 class AttackCalculatorTests : StringSpec({
-    "this test should pass" {
-        0 shouldBe 0
-    }
+    "defender is stronger enough to defend from damage" {
+        val attacker = Character(armorClass = 0, weaponDamage = 10, race = "elf", force = 1)
+        val defender = Character(armorClass = 22, weaponDamage = 10, race = "dwarf", force = 0)
 
-    "this test should fail" {
-        42 shouldBe 0
+        val attackCalculator = AttackCalculator()
+        val damage = attackCalculator.calculateDamage(atk = attacker, def = defender)
+        damage shouldBe 0
     }
 })
